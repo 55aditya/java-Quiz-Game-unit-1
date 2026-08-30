fe376e06c139aa8cf098c97cd12a5c1c685e2a05
omsaste <omsaste451@gmail.com>
2026-08-30 17:57:03 +0530

    Updated questions module for Om

--- QuizGame.java
+++ QuizGame.java
@@ -4,17 +4,25 @@ public class QuizGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // ==========================================
-        //  ADITYA - USER REGISTRATION
-        // ==========================================
+        // MEMBER 1 - ADITYA (Registration)
         System.out.println("===== JAVA QUIZ GAME =====");
-
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
+        System.out.println("Welcome " + name + "!\n");
 
-        System.out.println("Welcome " + name + "!");
-        System.out.println();
+        // ==========================================
+        // MEMBER 2 - OM: QUESTION AND ANSWER SET (UPDATED)
+        // ==========================================
+        String q1 = "What is the full form of JVM?";
+        String a1 = "Java Virtual Machine";
 
+        String q2 = "Which keyword is used to create a class?";
+        String a2 = "class";
+
+        String q3 = "Which data type stores whole numbers?";
+        String a3 = "int";
+
+        System.out.println("Questions loaded successfully!");
         sc.close();
     }
 }
